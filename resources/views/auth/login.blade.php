5239fadab0c8c80abf8b9036369e11f02a7351db
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2021-11-09 08:29:50 +0700

    update logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
                             <img src="{{ asset('assets/img/silacak.jpeg') }}" alt="logo" width="100"
-                                class="shadow-light rounded-circle" style="padding:10px;background: white;">
+                                class="shadow-light " style="padding:2px;background: white;border-radius:10px">
                         </div>
 
                         <div class="card card-primary">
