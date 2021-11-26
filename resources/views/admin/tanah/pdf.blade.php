b466eb4c4760a27cc113636c71af5db65b34c14a
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2021-11-26 15:59:48 +0700

    update pdf + excel

--- resources/views/admin/tanah/pdf.blade.php
+++ resources/views/admin/tanah/pdf.blade.php
@@ -3,7 +3,7 @@
     @php
             $user = Auth::user();
         @endphp
-    <title>Laporan Pendataan Asset Tanah {{ $user->upb_id != null ? $user->upb->name :  ($user->subunit_id != null ? $user->subunit->name : ($user->unit_id != null ? $user->unit->name : $user->bidang->name))}}</title>
+    <title>Laporan Pendataan Asset Tanah </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
